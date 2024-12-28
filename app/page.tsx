a25f601d937ac92a305f8aa946e6b46d434d8565
franco_n <clement.francois.developpeur@outlook.com>
2024-12-28 07:18:57 +0100

    Initialization of nextjs repository

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className="">
+        <p>Django Bnb</p>
+
+        <h2 className="text-airbnb">Django and Next rules</h2>
+      </main>
+    </div>
+  );
+}
